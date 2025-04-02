b1143e823103185540f36ffd6e2a4635af5531d8
GregoriusAviantoro <143041255+GregoriusAviantoro@users.noreply.github.com>
2025-04-02 01:22:16 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,10 +8,13 @@ export const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
-            <img src={darkLogo} alt="Otterance" className="h-16 w-auto mr-2" />
-            <div className="hidden md:flex ml-4 h-4 w-px bg-primary/20"></div>
-            <span className="hidden md:block ml-4 text-sm text-gray-600">
-              Zero Inbox Hassle. Close Deals Faster.
+          <div className="flex items-center space-x-2">
+          <img src={darkLogo} alt="Otterance" className="h-16 w-auto" />
+          <span className="text-lg font-semibold text-gray-800">Otterance</span>
+          </div>
+          <div className="hidden md:flex ml-4 h-4 w-px bg-primary/20"></div>
+          <span className="hidden md:block ml-4 text-sm text-gray-600">
+            Zero Inbox Hassle. Close Deals Faster.
             </span>
           </div>
           <div>
